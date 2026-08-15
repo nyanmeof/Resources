2fa8ef8c6a8675d63559591c3917c889aa49fc3b
nyanmeof <nyanle1907@gmail.com>
2026-08-15 14:44:33 +0700

    Update Xero.lua

--- KaitunBf/Xero.lua
+++ KaitunBf/Xero.lua
@@ -1,4 +1,5 @@
 setfpscap(10)
+game:GetService("RunService"):Set3dRenderingEnabled(false)
 script_key = "43ZOF5GL4P66P5PDZP46B5HF73RPN6P47T4PHYHX63WPB6HZ7W2LPKNA"
 getgenv().Shutdown = true
 getgenv().Configs = {
